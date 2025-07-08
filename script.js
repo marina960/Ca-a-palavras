5fcaba5716cbb9ec3baa9f25c5f6e60deeec863c
marina960 <marina.paiva@escola.pr.gov.br>
2025-07-08 13:52:14 +0000

    adicionei os códigos do alura

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
- const botaoMostraPalavras = document.querySelector(`#botao-palavrachave`);
- botaoMostraPalavras.addEventListener(´click´)
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+}
+<!-- código omitido -->
+
+<textarea id="entrada-de-texto" placeholder="Digite ou cole seu texto aqui"></textarea>
+
+<!-- código omitido -->
